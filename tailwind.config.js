7157ef5a0e58eed95abfe80a49fb34d91f887e49
kishan <kishanbapodra2002@gmail.com>
2022-09-16 19:06:34 +0400

    Add: galaxy font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        galaxy: ["SF Distant Galaxy", "regular"],
+      }
+    },
   },
   plugins: [],
 }
