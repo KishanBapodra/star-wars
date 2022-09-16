226c6c078df30c8e0ac24f1316839c9d90b020e0
kishan <kishanbapodra2002@gmail.com>
2022-09-17 02:27:08 +0400

    Create: main app page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import Card from './components/card';
+import { fetchPlanets } from './utils';
 
 const App = () => {
+    
+  //initial state before specific planets
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    fetchPlanets().then(res => setPlanets(res[0]));
+  },[]);
+  
   return (
-    <div className="text-center">
-      :: HELLO ::
+    <div className='bg-star-wars-1 bg-contain'>
+      <nav className="text-center p-10 min-h-fit font-galaxy text-4xl">
+        <h1 className='text-white'>STAR WARS</h1>
+        <div className='mt-10'>
+          {planets.map((planet,i) => {
+            if(planet.films.length !== 0)
+              return(<li key={i} className='font-sans text-sm list-none my-5'><Card planet={planet}/></li>)
+            else 
+              return(<p key={i}></p>)
+          })}
+        </div>
+      </nav>
     </div>
   );
 }
