253778af7c53c94efd9ef4e98e5a389a54a209b0
kishan <kishanbapodra2002@gmail.com>
2022-09-17 20:21:14 +0400

    Final Product

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# star-wars
-A website with info of planets in star wars that has people having reptiles, represented in cards.
+# STAR WARS
+
+A website with info of planets in star wars.
+
+Toggle feature:
+    i)  Created cards with info of planets, when they were created, name, films in which they have appeared and the climate.
+    ii) Cards of planets that are the homeworlds of species of reptilian classification and designation.
--- src/App.js
+++ src/App.js
@@ -1,43 +1,61 @@
 import React, { useState, useEffect } from 'react';
 import Card from './components/card';
-import { fetchPlanets, fetchSpecies } from './utils';
+import {fetchPlanets, fetchPlnts, fetchSpecies } from './utils';
 import { motion } from 'framer-motion';
 
 const App = () => {
     
-  //initial state before specific planets
   const [planets, setPlanets] = useState([]);
+  const [reptileSpecies, setSpecies] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [toggle, setToggle] = useState(false);
+
 
   useEffect(() => {
-    fetchPlanets().then(res => setPlanets(res[0]));
-    fetchSpecies();
-  },[]);
+    if(!toggle)
+      fetchPlanets().then(res => setPlanets(res[0]));
+    else
+      fetchSpecies().then(res => setSpecies(res)).then(() => setLoading(false));
+  },[toggle]);
+
+  useEffect(() => {
+    let url = [];
+    if(!isLoading) {
+      reptileSpecies.forEach(ele => {
+          url.push(ele.homeworld);
+      });
+    }
+    fetchPlnts(url).then(res => setPlanets(res));
+
+  }, [isLoading, reptileSpecies, toggle]);
   
-  return (
-    <div className='bg-star-wars-1 bg-contain'>
-      <nav className="text-center p-10 min-h-fit font-galaxy text-4xl">
-        <h1 className='text-white'>STAR WARS</h1>
-        <div className='mt-10'>
-          {planets.map((planet,i) => {
-            if(planet.films.length !== 0)
-              return(
-                <motion.li
-                  initial={{y: -50, x: '-100vw'}}
-                  animate={{y: 0, x: 0}}
-                  transition={{delay: 1, duration: 0.5, stiffness: 50, type: 'spring'}}
-                  key={i}
-                  className='font-sans text-sm list-none my-5'
-                >
-                  <Card planet={planet}/>
-                </motion.li>
-              )
-            else 
-              return(<p key={i}></p>)
-          })}
-        </div>
-      </nav>
-    </div>
-  );
+  if(planets !== null) {
+    return (
+      <div className='bg-star-wars-1 min-h-screen bg-auto'>
+        <nav className="text-center p-10 min-h-fit font-galaxy text-4xl">
+          <div className='flex justify-between items-center'>
+            <h1 className='text-white'>STAR WARS</h1>
+            <button className='p-2 text-white text-sm bg-neutral-500 rounded-full' onClick={() => setToggle(!toggle)}>toggle</button>
+          </div>
+          <div className='mt-10'>
+            {planets.map((planet,i) => {
+                return(
+                  <motion.li
+                    initial={{y: -50, x: '-100vw'}}
+                    animate={{y: 0, x: 0}}
+                    transition={{delay: 0.25, duration: 0.5, stiffness: 50, type: 'spring'}}
+                    key={i}
+                    className='font-sans text-sm list-none my-5'
+                  >
+                    <Card planet={planet} toggle={toggle}/>
+                  </motion.li>
+                )
+            })}
+          </div>
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default App;
